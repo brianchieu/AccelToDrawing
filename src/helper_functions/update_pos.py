c627cff65c908884e2183cbfec14a934d90264b7
Ashwin Karthik Tamilselvan <at3103@columbia.edu>
2017-05-11 21:30:49 -0400

    Update update_pos.py

--- src/helper_functions/update_pos.py
+++ src/helper_functions/update_pos.py
@@ -23,11 +23,11 @@ def compute_position(accel, init_position, delta_t, u_x, u_y):
 	delta_t_sq_half = 0.5 * (delta_t**2)
 
 	for x,y,z in accel:
-		v_x = (u_x + delta_t * x) if x != 0.0  else 0.0
-		v_y = (u_y + delta_t * y) if y != 0.0 else 0.0
+		v_x = (u_x + delta_t * x) if x != 0.0  else 0.0		#Equate final velocity to zero if acceleration is zero
+		v_y = (u_y + delta_t * y) if y != 0.0 else 0.0		#Equate final velocity to zero if acceleration is zero
 		
-		s_x += (u_x * delta_t + x * delta_t_sq_half)
-		s_y += (u_y * delta_t + y * delta_t_sq_half)
+		s_x += (u_x * delta_t + x * delta_t_sq_half)		#Compute displacement using : s = ut + 1/2*a*t^2
+		s_y += (u_y * delta_t + y * delta_t_sq_half)		#Compute displacement using : s = ut + 1/2*a*t^2
 		u_x, u_y = v_x,v_y
 
 	return [s_x, s_y], v_x, v_y
